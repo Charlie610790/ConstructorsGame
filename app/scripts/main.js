cce7a49a3bcbac6f2288cbab486e6310050dd931
Charlie Jones <myupstateproperties@gmail.com>
2014-05-20 15:23:02 -0400

    fixing grunt errors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,15 @@
 $('.startbutton').click(function(){
-  player = new Hero();
-  enemy = new Enemy()
+    player = new Hero();
+    enemy = new Enemy();
  
-  $('.startgame').remove();
+    $('.startgame').remove();
  
-  $('.battleground').addClass('active');
+    $('.battleground').addClass('active');
 
 });
 
 function Hero () {
-    this.name = "HERO"
+    this.name = 'HERO';
     this.health = 100;
  
     this.punch = function(target) {
@@ -25,12 +25,12 @@ function Hero () {
     };
 
     this.life = function(target) {
-        target.health = target.healty = 100;
+        target.health = target.health = 100;
     };
 }
 
 function Enemy (level) {
-    this.name = "Enemy"
+    this.name = 'Enemy';
     this.health = 100;
  
     this.punch = function(target) {
@@ -46,72 +46,72 @@ function Enemy (level) {
     };
 
     this.life = function(target) {
-        target.health = target.healty = 100;
+        target.health = target.health = 100;
     };
 }
 
 function playerStats (player) {
-  if (player.health < 1) {
-    $('.p1-playerhealth').text("YOU DIED").addClass('dead')
-    $('.continue').html('<a href=".">Click Here To Continue</a>').addClass('active')
+    if (player.health < 1) {
+        $('.p1-playerhealth').text('YOU DIED').addClass('dead');
+        $('.continue').html('<a href=".">Click Here To Continue</a>').addClass('active');
 
-    } 
+    }
     else {
-    $('.p1-playerhealth').text("Health: " + player.health)
+        $('.p1-playerhealth').text('Health: ' + player.health);
     }
 }
  
-  function enemyStats (player) {
-  if (player.health < 1) {
-    $('.p2-playerhealth').html("<span class='dead'>BOSS DEFEATED</span>")
+function enemyStats (player) {
+    if (player.health < 1) {
+        $('.p2-playerhealth').html('<span class="dead">BOSS DEFEATED</span>');
     
-    } 
+    }
     else {
-    $('.p2-playerhealth').text("Health: " + player.health)
+        $('.p2-playerhealth').text('Health: ' + player.health);
     }
 }
 
 $('.p1-punch-button').click(function(){
-  player.punch(enemy);
-  playerStats(player);
-  enemyStats(enemy); 
-})
+    player.punch(enemy);
+    playerStats(player);
+    enemyStats(enemy);
+});
 
 $('.p1-kick-button').click(function(){
-  player.kick(enemy);
-  playerStats(player);
-  enemyStats(enemy); 
-})
+    player.kick(enemy);
+    playerStats(player);
+    enemyStats(enemy);
+});
 
 $('.p1-special-button').click(function(){
-  player.special(enemy);
-  playerStats(player);
-  enemyStats(enemy); 
-})
+    player.special(enemy);
+    playerStats(player);
+    enemyStats(enemy);
+});
 
 $('.p1-life-button').click(function(){
-  player.life(enemy);
-  playerStats(player);
-  enemyStats(enemy); 
-})
+    player.life(enemy);
+    playerStats(player);
+    enemyStats(enemy);
+});
 
 $('.p2-kick-button').click(function(){
-  enemy.kick(player);
-  playerStats(player);
-  enemyStats(enemy); 
-})
+    enemy.kick(player);
+    playerStats(player);
+    enemyStats(enemy);
+});
 
 $('.p2-punch-button').click(function(){
-  enemy.punch(player);
-  playerStats(player);
-  enemyStats(enemy); 
-})
+    enemy.punch(player);
+    playerStats(player);
+    enemyStats(enemy);
+});
 
 $('.p2-special-button').click(function(){
-  enemy.special(player);
-  playerStats(player);
-  enemyStats(enemy); 
-})
+    enemy.special(player);
+    playerStats(player);
+    enemyStats(enemy);
+});
 
 $('.p1-playername').map(function() {
         $(this).text(Hero.name);
